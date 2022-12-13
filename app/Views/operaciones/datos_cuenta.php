6553f513aacf2fddc21238d07f7ae1c011e2b45c
Aley Colen <aleycolen1998.2@gmail.com>
2022-12-13 12:36:51 -0600

    agregue pagina para ver cuentas

---
+++ app/Views/operaciones/datos_cuenta.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de cuentas</title>
+</head>
+<body>
+<div class="table-responsive">
+        <table class="table table-primary">
+            <thead>
+                <tr>
+                    <th scope="col">No. Cuenta</th>
+                    <th scope="col">Tipo de cuenta</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($cuentas as $cuenta):?>
+                <tr class="">
+                    <td><?=$cuenta['numero_cuenta']?></td>
+                    <td><?=$cuenta['tipo_cuenta']?></td>
+                </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
